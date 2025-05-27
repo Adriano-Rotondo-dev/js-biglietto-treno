cf105365da2d7fe0a2344b0fd66bd39e1e612520
Adriano Rotondo <adriano.rotondo.dev@gmail.com>
2025-05-27 16:43:48 +0200

    Add initial implementation for Pari e Dispari game logic

---
+++ bonus/pari/pari.js
@@ -0,0 +1,19 @@
+/* pari o dispari: chiedere all'utente se vuole pari o dispari e un numero intero compreso tra 1 e 9. 
+ Generare un numero casuale compreso tra 1 e 9, che sarà la giocata del computer.
+ Stabilire e comunicare chi ha vinto secondo le regole di pari o dispari. */
+
+ // chiedere all'utente di scegliere tra pari o dispari
+
+ const userChoice = (prompt("Pari o dispari?"));
+ console.log("Hai scelto " + userChoice);
+
+ //chiedere all'utente un numero intero compreso tra 1 e 9
+
+ const userNumber = Number(prompt("Scegli un numero intero compreso tra 1 e 9 "));
+ console.log("Hai scelto " + userNumber);
+
+ //Generare un numero casuale compreso tra 1 e 9 (giocata del computer)
+
+
+
+ //Stabilire e comunicare chi ha vinto secondo le regole di pari o dispari
